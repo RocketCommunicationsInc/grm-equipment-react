25181e09fb2b8f12f09412cb0a8e1ec2cfc4e642
Tom Neal <thomasevanneal@gmail.com>
2022-09-07 16:57:29 -0500

    Set up CRA

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
+import '@astrouxds/astro-web-components/dist/astro-web-components/astro-web-components.css';
 import './App.css';
+import { RuxGlobalStatusBar } from '@astrouxds/react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RuxGlobalStatusBar
+        include-icon="true"
+        app-domain="Astro"
+        app-name="Dashboard"
+        app-version="4.0 Alpha"
+        menu-icon="apps"
+        username=""
+        app-state-color=""
+        app-state=""
+      ></RuxGlobalStatusBar>
+      <rux-button>Button text</rux-button>
     </div>
   );
 }
