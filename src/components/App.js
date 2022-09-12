aa7ba5a3f47a5352dcc749793df449f8b258f135
Terry Roe <terryroe@gmail.com>
2022-09-12 10:23:42 -0600

    Add GlobalStatusBar (#1)
    * Add GlobalStatusBar
    
    * Add global nav menu
    
    * Move monitoring badge down some
    
    * Remove unused file
    
    * make status bar higher, give more space for icons
    
    * Add cursor pointer to main menu icon

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,17 @@
 import '@astrouxds/astro-web-components/dist/astro-web-components/astro-web-components.css';
 import '../css/App.scss';
-import { RuxGlobalStatusBar } from '@astrouxds/react';
+import GlobalStatusBar from './GlobalStatusBar/GlobalStatusBar';
 import EquipmentContainer from './EquipmentContainer/EquipmentContainer';
 
 function App() {
   return (
-    <div className="App">
-      <RuxGlobalStatusBar
-        include-icon="true"
-        app-domain="Astro"
-        app-name="Dashboard"
-        app-version="4.0 Alpha"
-        menu-icon="apps"
-        username=""
-        app-state-color=""
-        app-state=""
-      ></RuxGlobalStatusBar>
+    <>
+      <GlobalStatusBar />
       <main>
         <nav className="main-menu">Menu</nav>
         <EquipmentContainer>Equipment</EquipmentContainer>
       </main>
-    </div>
+    </>
   );
 }
 
