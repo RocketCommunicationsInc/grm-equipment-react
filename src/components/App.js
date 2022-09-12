e3f20f9b96cd207eb19c3342bb4903702082dc98
atulmri123 <atul.murali@rocketcom.com>
2022-09-12 15:44:16 -0700

    Merge branch 'sidebar'

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import '@astrouxds/astro-web-components/dist/astro-web-components/astro-web-comp
 import '../css/App.scss';
 import GlobalStatusBar from './GlobalStatusBar/GlobalStatusBar';
 import EquipmentContainer from './EquipmentContainer/EquipmentContainer';
+import SidebarTree from './Sidebar/SidebarTree';
 import React from 'react';
 
 function App() {
@@ -9,7 +10,9 @@ function App() {
     <>
       <GlobalStatusBar />
       <main>
-        <nav className="main-menu">Menu</nav>
+        <nav className="main-menu">
+          <SidebarTree />
+        </nav>
         <EquipmentContainer>Equipment</EquipmentContainer>
       </main>
     </>
