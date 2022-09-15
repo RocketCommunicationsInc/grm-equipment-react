bbfe3cb4bfbea7f7ab3419d013b6db296d106434
Terry Roe <terryroe@gmail.com>
2022-09-15 16:22:25 -0600

    Add alert log entry

--- src/components/EquipmentDetails/EquipmentAlerts.js
+++ src/components/EquipmentDetails/EquipmentAlerts.js
@@ -1,4 +1,25 @@
-import { RuxOption, RuxSelect } from '@astrouxds/react';
+import { RuxCheckbox, RuxOption, RuxSelect, RuxStatus } from '@astrouxds/react';
+
+const Alert = ({ selected, status, message, category, timestamp }) => {
+  return (
+    <>
+      <li className="alert-log__event alert-log--collapsed">
+        <div className="alert-log__event__select">
+          <RuxCheckbox className="rux-checkbox" checked={selected} />
+        </div>
+        <div className="alert-log__event__status">
+          <RuxStatus status={status} />
+        </div>
+        <div className="alert-log__event__message">{message}</div>
+        <div className="alert-log__event__category">{category}</div>
+        <div className="alert-log__event__timestamp">{timestamp}</div>
+      </li>
+      <div className="alert-log__detail">
+        <div></div>
+      </div>
+    </>
+  );
+};
 
 const EquipmentAlerts = () => {
   return (
@@ -42,6 +63,55 @@ const EquipmentAlerts = () => {
                 </RuxSelect>
               </div>
             </div>
+          </div>
+          <div className="alert-log">
+            <header className="alert-log-header">
+              <div className="alert-log__header-labels">
+                <div className="alert-log__event__select">Select All</div>
+                <div className="alert-log__event__status"></div>
+                <div className="alert-log__event__message">Message</div>
+                <div className="alert-log__event__category">Category</div>
+                <div className="alert-log__event__timestamp">Time</div>
+              </div>
+            </header>
+
+            <ol className="alert-log__events">
+              <Alert
+                selected={true}
+                status="critical"
+                message="Antenna DGS 2 - Weak signal"
+                category="Hardware"
+                timestamp="17:58:37"
+              />
+              <Alert
+                selected={false}
+                status="critical"
+                message="USA-177 - Solar panel misalignment"
+                category="Spacecraft"
+                timestamp="17:58:32"
+              />
+              <Alert
+                selected={false}
+                status="critical"
+                message="Black FEP 121 - Offline"
+                category="Software"
+                timestamp="17:58:28"
+              />
+              <Alert
+                selected={false}
+                status="serious"
+                message="Workstation 134 - Offline"
+                category="Hardware"
+                timestamp="17:58:17"
+              />
+              <Alert
+                selected={false}
+                status="serious"
+                message="Antenna DGS 2 - Offline"
+                category="Hardware"
+                timestamp="17:58:12"
+              />
+            </ol>
           </div>
         </div>
       </div>
