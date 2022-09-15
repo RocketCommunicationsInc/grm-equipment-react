06f75cab43b6f3bc9065659cd3d34e33e44e36a6
Terry Roe <terryroe@gmail.com>
2022-09-15 13:58:47 -0600

    Add alert select lists

--- src/components/EquipmentDetails/EquipmentAlerts.js
+++ src/components/EquipmentDetails/EquipmentAlerts.js
@@ -1,10 +1,48 @@
+import { RuxOption, RuxSelect } from '@astrouxds/react';
+
 const EquipmentAlerts = () => {
   return (
     <>
       <div className="grid-zone grid-zone--equipment-alerts grid-zone--fixed">
         <div className="grid-zone__label">Alerts</div>
         <div className="grid-zone__content">
-          <grm-alerts log="{{alertsLog}}"></grm-alerts>
+          <div className="alert-bin-header">
+            <div className="alert-summary">
+              <span className="alert-count"> 12 </span> Active Alerts
+            </div>
+
+            <div className="alert-filters">
+              <div className="alert-filter">
+                <label htmlFor="statusFilter">Severity</label>
+                <RuxSelect
+                  input-id="statusFilter"
+                  className="rux-select"
+                  required={false}
+                  value="all"
+                >
+                  <RuxOption value="all" label="All" />
+                  <RuxOption value="critical" label="Critical" />
+                  <RuxOption value="serious" label="Serious" />
+                  <RuxOption value="caution" label="Caution" />
+                </RuxSelect>
+              </div>
+
+              <div className="alert-filter">
+                <label htmlFor="categoryFilter">Category</label>
+                <RuxSelect
+                  input-id="categoryFilter"
+                  className="rux-select"
+                  required={false}
+                  value="all"
+                >
+                  <RuxOption value="all" label="All" />
+                  <RuxOption value="hardware" label="Hardware" />
+                  <RuxOption value="software" label="Software" />
+                  <RuxOption value="spacecraft" label="Spacecraft" />
+                </RuxSelect>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
