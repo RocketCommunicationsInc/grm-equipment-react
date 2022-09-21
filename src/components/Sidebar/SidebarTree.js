5221465fe1ac346d668553827d6c71534a876dbc
atulmri123 <atul.murali@rocketcom.com>
2022-09-21 12:17:22 -0700

    Remove unnecessary ternary operators

--- src/components/Sidebar/SidebarTree.js
+++ src/components/Sidebar/SidebarTree.js
@@ -9,54 +9,42 @@ const SidebarTree = () => {
   return (
     <>
       <RuxTree>
-        {sidebarObjects
-          ? sidebarObjects.map(function (a) {
-              return (
-                <RuxTreeNode key={a.label}>
-                  {a.status ? <RuxStatus status={a.status} /> : ''}
-                  {a.label}
+        {sidebarObjects &&
+          sidebarObjects.map(function (a) {
+            return (
+              <RuxTreeNode key={a.label}>
+                {a.status && <RuxStatus status={a.status} />}
+                {a.label}
 
-                  {a.children
-                    ? a.children.map(function (b) {
-                        return (
-                          <RuxTreeNode key={b.label} slot="node">
-                            {b.status ? <RuxStatus status={b.status} /> : ''}{' '}
-                            {b.label}
-                            {b.children
-                              ? b.children.map(function (c) {
-                                  return (
-                                    <RuxTreeNode key={c.label} slot="node">
-                                      {c.status ? (
-                                        <RuxStatus status={c.status} />
-                                      ) : (
-                                        ''
-                                      )}{' '}
-                                      {c.label}
-                                      {c.children
-                                        ? c.children.map(function (d) {
-                                            return (
-                                              <RuxTreeNode
-                                                key={d.label}
-                                                slot="node"
-                                              >
-                                                <RuxStatus status={d.status} />
-                                                {d.label}
-                                              </RuxTreeNode>
-                                            );
-                                          })
-                                        : ''}
-                                    </RuxTreeNode>
-                                  );
-                                })
-                              : ''}
-                          </RuxTreeNode>
-                        );
-                      })
-                    : ''}
-                </RuxTreeNode>
-              );
-            })
-          : ''}
+                {a.children &&
+                  a.children.map(function (b) {
+                    return (
+                      <RuxTreeNode key={b.label} slot="node">
+                        {b.status && <RuxStatus status={b.status} />} {b.label}
+                        {b.children &&
+                          b.children.map(function (c) {
+                            return (
+                              <RuxTreeNode key={c.label} slot="node">
+                                {c.status && <RuxStatus status={c.status} />}
+                                {c.label}
+                                {c.children &&
+                                  c.children.map(function (d) {
+                                    return (
+                                      <RuxTreeNode key={d.label} slot="node">
+                                        <RuxStatus status={d.status} />
+                                        {d.label}
+                                      </RuxTreeNode>
+                                    );
+                                  })}
+                              </RuxTreeNode>
+                            );
+                          })}
+                      </RuxTreeNode>
+                    );
+                  })}
+              </RuxTreeNode>
+            );
+          })}
       </RuxTree>
     </>
   );
