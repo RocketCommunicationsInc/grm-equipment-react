3d698f0a68639ffb07e50358f61ef5bd69c5aebe
Terry Roe <terryroe@gmail.com>
2022-09-22 10:01:30 -0600

    Implement Contacts summary info

--- src/components/EquipmentDetails/EquipmentContacts.js
+++ src/components/EquipmentDetails/EquipmentContacts.js
@@ -1,10 +1,39 @@
+import { RuxOption, RuxSelect } from '@astrouxds/react';
+import './EquipmentContacts.scss';
+
 const EquipmentContacts = () => {
   return (
     <>
       <div className="grid-zone grid-zone--equipment-contacts grid-zone--fixed">
         <div className="grid-zone__label">Current Contacts</div>
         <div className="grid-zone__content">
-          <current-contacts log="[[contactsLog]]"></current-contacts>
+          <div className="contact-bin-header">
+            <div className="contact-summary contact-summary--all">
+              <span className="contact-count"> 4 </span> Contacts
+            </div>
+            <div className="contact-summary contact-summary--failed">
+              <span className="contact-count"> 1 </span> Failed
+            </div>
+            <div className="contact-summary contact-summary--executing">
+              <span className="contact-count"> 3 </span> Executing
+            </div>
+            <div className="contact-filters">
+              <div className="contact-filter">
+                <label htmlFor="stateFilter">Status</label>
+                <RuxSelect
+                  input-id="stateFilter"
+                  className="rux-select"
+                  required={false}
+                  value="all"
+                >
+                  <RuxOption value="all" label="All" />
+                  <RuxOption value="critical" label="Critical" />
+                  <RuxOption value="serious" label="Serious" />
+                  <RuxOption value="caution" label="Caution" />
+                </RuxSelect>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
