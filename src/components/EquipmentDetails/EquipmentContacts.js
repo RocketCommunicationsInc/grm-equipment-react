48db4d4902a34222966d3a405383ddc42f38abde
Terry Roe <terryroe@gmail.com>
2022-09-23 10:48:28 -0600

    Roll up work-to-date on Current Contacts

--- src/components/EquipmentDetails/EquipmentContacts.js
+++ src/components/EquipmentDetails/EquipmentContacts.js
@@ -1,5 +1,101 @@
-import { RuxOption, RuxSelect } from '@astrouxds/react';
+import { RuxOption, RuxSelect, RuxStatus } from '@astrouxds/react';
 import './EquipmentContacts.scss';
+
+const Contact = ({
+  status,
+  name,
+  ground,
+  equipment,
+  state,
+  step,
+  begin,
+  end,
+}) => {
+  function getStatus() {
+    if (state === 'executing') return 'normal';
+    if (state === 'failed') return 'serious';
+    return 'off';
+  }
+  status = getStatus();
+
+  return (
+    <>
+      <li className="contact-log__event contact-log--collapsed">
+        <div className="contact-log__event__status">
+          <RuxStatus status={status} />
+        </div>
+        <div className="contact-log__event__name">{name}</div>
+        <div className="contact-log__event__ground">{ground}</div>
+        <div className="contact-log__event__equipment">{equipment}</div>
+        <div
+          className={`contact-log__event__state contact-log__event__state--${status}`}
+        >
+          {state} (Step: {step})
+        </div>
+        <div className="contact-log__event__timestamp">
+          <span>{begin}</span>
+          &ndash;
+          <span>{end}</span>
+        </div>
+      </li>
+    </>
+  );
+};
+
+const contactLogs = [
+  {
+    contactId: '80e5654b-df70-5a9c-85dd-75541a7cafae',
+    contactName: '22683',
+    contactGround: 'HTS',
+    contactEquipment: 'ANT43 VAFB1 SFEP227CH1 ECEU6 WS402 USP177',
+    contactState: 'executing',
+    contactStep: 'Downlink',
+    contactDetail:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    contactBeginTimestamp: 1571086435343,
+    contactEndTimestamp: 1571088368326,
+    expanded: false,
+  },
+  {
+    contactId: '40e5654b-df70-5a9c-85dd-75541a7cafae',
+    contactName: '68112',
+    contactGround: 'VTS',
+    contactEquipment: 'ANT74 BAFB1 SFEP299CH1 ECEU6 WS305 USP451',
+    contactState: 'failed',
+    contactStep: 'AOS',
+    contactDetail:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    contactBeginTimestamp: 1571088235343,
+    contactEndTimestamp: 1571088398326,
+    expanded: false,
+  },
+  {
+    contactId: '70e5654b-df70-5a9c-85dd-75541a7cafae',
+    contactName: '16834',
+    contactGround: 'DGS',
+    contactEquipment: 'ANT73 PAFB1 SFEP149CH1 ECEU6 WS167 USP182',
+    contactState: 'executing',
+    contactStep: 'Uplink',
+    contactDetail:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    contactBeginTimestamp: 1571088635343,
+    contactEndTimestamp: 1571088768326,
+    expanded: false,
+  },
+  {
+    contactId: '00e5654b-df70-5a9c-85dd-75541a7cafae',
+    contactName: '62346',
+    contactGround: 'TCS',
+    contactEquipment: 'ANT52 SAFB1 SFEP374CH1 ECEU6 WS481 USP342',
+    contactState: 'executing',
+    contactStep: 'Downlink',
+    contactDetail:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    contactBeginTimestamp: 1571085435343,
+    contactEndTimestamp: 1571089368326,
+    expanded: false,
+  },
+];
 
 const EquipmentContacts = () => {
   return (
@@ -34,21 +130,38 @@ const EquipmentContacts = () => {
               </div>
             </div>
           </div>
-          <div class="contact-log">
-            <header class="contact-log-header">
-              <div class="contact-log__header-labels">
-                <div class="contact-log__event__status"></div>
-                <div class="contact-log__event__name">Name</div>
-                <div class="contact-log__event__ground">GS</div>
-                <div class="contact-log__event__equipment">
+          <div className="contact-log">
+            <header className="contact-log-header">
+              <div className="contact-log__header-labels">
+                <div className="contact-log__event__status"></div>
+                <div className="contact-log__event__name">Name</div>
+                <div className="contact-log__event__ground">GS</div>
+                <div className="contact-log__event__equipment">
                   Equipment String
                 </div>
-                <div class="contact-log__event__state">Status</div>
-                <div class="contact-log__event__timestamp">AOS &ndash; LOS</div>
+                <div className="contact-log__event__state">Status</div>
+                <div className="contact-log__event__timestamp">
+                  AOS &ndash; LOS
+                </div>
               </div>
             </header>
 
-            <ol class="contact-log__events"></ol>
+            <ol className="contact-log__events">
+              {contactLogs.map((log) => {
+                return (
+                  <Contact
+                    key={log.contactId}
+                    name={log.contactName}
+                    ground={log.contactGround}
+                    equipment={log.contactEquipment}
+                    state={log.contactState}
+                    step={log.contactStep}
+                    begin={log.contactBeginTimestamp}
+                    end={log.contactEndTimestamp}
+                  />
+                );
+              })}
+            </ol>
           </div>
         </div>
       </div>
