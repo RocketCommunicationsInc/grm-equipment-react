e962a2df5364d148bb5583bc48079204de9f2a66
Paul Cristea <paul.cristea@rocketcom.com>
2022-10-04 10:47:04 -0700

    move Context to Layout

--- src/components/App.js
+++ src/components/App.js
@@ -2,36 +2,17 @@ import './App.scss';
 import GlobalStatusBar from './GlobalStatusBar/GlobalStatusBar';
 import EquipmentContainer from './EquipmentContainer/EquipmentContainer';
 import SidebarTree from './Sidebar/SidebarTree';
-import { useState } from 'react';
-import { DataContext } from '../DataContext';
-import { mainData } from '../services/data';
 
 function App() {
-  const [dataContext, setDataContext] = useState({
-    data: mainData,
-    notifiyUpdate: customNotifiyUpdate,
-  });
-
-  function customNotifiyUpdate() {
-    setDataContext((oldContext) => ({
-      data: oldContext.data,
-      notifiyUpdate: oldContext.notifiyUpdate,
-    }));
-  }
-
-  mainData.notifiyUpdate = customNotifiyUpdate;
-
   return (
     <>
-      <DataContext.Provider value={dataContext}>
-        <GlobalStatusBar />
-        <main>
-          <nav className="main-menu">
-            <SidebarTree />
-          </nav>
-          <EquipmentContainer>Equipment</EquipmentContainer>
-        </main>
-      </DataContext.Provider>
+      <GlobalStatusBar />
+      <main>
+        <nav className="main-menu">
+          <SidebarTree />
+        </nav>
+        <EquipmentContainer>Equipment</EquipmentContainer>
+      </main>
     </>
   );
 }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,27 @@
 import '@astrouxds/astro-web-components/dist/astro-web-components/astro-web-components.css';
 import './Layout.scss';
-
-import React from 'react';
+import { DataContext } from '../DataContext';
+import { mainData } from '../services/data';
+import { useState } from 'react';
 
 const Layout = ({ children }) => {
-  return <>{children}</>;
+  const [dataContext, setDataContext] = useState({
+    data: mainData,
+    notifiyUpdate: customNotifiyUpdate,
+  });
+
+  function customNotifiyUpdate() {
+    setDataContext((oldContext) => ({
+      data: oldContext.data,
+      notifiyUpdate: oldContext.notifiyUpdate,
+    }));
+  }
+
+  mainData.notifiyUpdate = customNotifiyUpdate;
+
+  return (
+    <DataContext.Provider value={dataContext}>{children}</DataContext.Provider>
+  );
 };
 
 export default Layout;
